ae9f3821fad1943e0de9385f239561551954cfe9
DaV <DaV@DESKTOP-GH660AU>
2024-05-30 21:55:58 +0200

    Defined dbContext, moved models to dbsets folder

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using DeliveryApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Privilege> Priviliges { get; set; }
+
+        public DbSet<Delivery> Deliveries { get; set; }
+        public DbSet<DeliveryStatusUpdate> DeliveryStatusUpdates { get; set; }
+
+        public DbSet<Complaint> Complaints { get; set; }
+
+        public DbSet<Report> Reports { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlite("Data Source=products.db");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
