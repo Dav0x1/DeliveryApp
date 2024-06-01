fdd91770506b73a4215b6cc3839adf8fadfe6f8c
Paulina Czyżewska <82710@student.pb.edu.pl>
2024-06-02 01:01:54 +0200

    register view

--- Views/RegisterView.xaml.cs
+++ Views/RegisterView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//using DeliveryApp.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,67 @@ namespace DeliveryApp.Views
     /// </summary>
     public partial class RegisterView : UserControl
     {
-        public RegisterView()
-        {
-            InitializeComponent();
-        }
-    }
+		//private readonly UserService _userService;
+
+		public RegisterView()
+		{
+			InitializeComponent();
+			//_userService = new UserService(); // Inicjalizacja usługi użytkownika
+		}
+		private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+		{
+			PasswordTextBox.Text = PasswordBox.Password;
+			ConfirmPasswordTextBox.Text = ConfirmPasswordBox.Password;
+		}
+
+		private void ShowPasswordCheckBox_Checked(object sender, RoutedEventArgs e)
+		{
+			PasswordBox.Visibility = Visibility.Collapsed;
+			PasswordTextBox.Visibility = Visibility.Visible;
+			PasswordTextBox.Text = PasswordBox.Password;
+
+			ConfirmPasswordBox.Visibility = Visibility.Collapsed;
+			ConfirmPasswordTextBox.Visibility = Visibility.Visible;
+			ConfirmPasswordTextBox.Text = ConfirmPasswordBox.Password;
+		}
+
+		private void ShowPasswordCheckBox_Unchecked(object sender, RoutedEventArgs e)
+		{
+			PasswordBox.Visibility = Visibility.Visible;
+			PasswordTextBox.Visibility = Visibility.Collapsed;
+			PasswordBox.Password = PasswordTextBox.Text;
+
+			ConfirmPasswordBox.Visibility = Visibility.Visible;
+			ConfirmPasswordTextBox.Visibility = Visibility.Collapsed;
+			ConfirmPasswordBox.Password = ConfirmPasswordTextBox.Text;
+		}
+
+		private async void RegisterButton_Click(object sender, RoutedEventArgs e)
+		{
+			string username = UsernameTextBox.Text;
+			string password = PasswordBox.Password;
+			string confirmPassword = ConfirmPasswordBox.Password;
+
+			// Ewentualna dodatkowa walidacja - TUTAJ
+
+			// Sprawdzenie czy hasła zgodne
+			if (password != confirmPassword)
+			{
+				MessageBox.Show("Passwords do not match!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+				return;
+			}
+
+			// Rejestracja 
+			/*bool registrationResult = await _userService.RegisterUserAsync(username, password);
+
+			if (registrationResult)
+			{
+				MessageBox.Show("Registration successful!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+			}
+			else
+			{
+				MessageBox.Show("Registration failed! Username already exists.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+			}*/
+		}
+	}
 }
