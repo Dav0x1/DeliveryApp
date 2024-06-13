9feddd9b815fa75ad1a6f65b9d2efcfad60d0a12
DaV <DaV@DESKTOP-GH660AU>
2024-06-14 00:08:39 +0200

    static helper for getting translation

---
+++ Helpers/LanguageResources.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DeliveryApp.Helpers
+{
+    public static class LanguageResources
+    {
+        private static readonly ResourceDictionary LanguageDictionary = (ResourceDictionary)Application.Current.Resources["LanguageDictionary"];
+
+        public static string GetStringValue(string key)
+        {
+            if(LanguageDictionary is null)
+                return string.Empty;
+
+            return LanguageDictionary.Contains(key) ? LanguageDictionary[key] as string : string.Empty;
+        }
+    }
+}
